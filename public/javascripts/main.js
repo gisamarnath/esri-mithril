88b094df0b6b47d9017233105cf2f64dcf22f04a
Brad Janke <brad.janke@gmail.com>
2015-06-09 13:44:50 -0500

    Adds main css. Fixed error page. Adds esri-leaflet. Working base

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,22 +1,20 @@
 // app namespace
 var em = {};
 
-require(["esri/map", "dojo/domReady!"], function(Map) { 
-    'use strict';
+$(function() {
+    var map = L.map('map').setView([45.528, -122.680], 13);
 
-    var map_div = $('#map');
-    var win = $(window);
+    L.esri.basemapLayer("Gray").addTo(map);
 
-    map_div.height(win.height());
+    var parks = new L.esri.FeatureLayer("http://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Portland_Parks/FeatureServer/0", {
+    style: function () {
+      return { color: "#70ca49", weight: 2 };
+    }
+    }).addTo(map);
 
-    $(window).resize(function () {
-        map_div.height(win.height());
-    });
+    var popupTemplate = "<h3>{NAME}</h3>{ACRES} Acres<br><small>Property ID: {PROPERTYID}<small>";
 
-    em.map = new Map('map', {
-        center: [-118, 34.5],
-        zoom: 8,
-        basemap: "topo",
-        autoResize: true
+    parks.bindPopup(function(feature){
+    return L.Util.template(popupTemplate, feature.properties)
     });
 });
