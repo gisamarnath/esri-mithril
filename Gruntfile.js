e19f78166a345b9f8b8af1d3a60f6195a46077a1
Brad Janke <brad.janke@gmail.com>
2015-06-09 14:15:26 -0500

    Adds dist/ to ignore. Updates Grunt to build and push css and js files to untracked dist/.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,28 +2,60 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        jshint: {
-          all: ['Gruntfile.js', 'package.json', 'public/javascripts/**/*.js']
+
+        jshint : {
+            all: ['Gruntfile.js', 'package.json', 'public/javascripts/**/*.js']
         },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: [
-                  'public/javascripts/**/*.js'
-                ],
-                dest: 'build/main.min.js'
+
+        concat : {
+          options: {
+            // define a string to put between each file in the concatenated output
+            separator: ';',
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:mm") %> */\n'
+          },
+          dist: {
+            // the files to concatenate
+            src: ['public/javascripts/**/*.js'],
+            // the location of the resulting JS file
+            dest: 'dist/<%= pkg.name %>.js'
+          }
+        },
+
+        uglify : {
+          options: {
+              banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:mm") %> */\n'
+          },
+
+          dist: {
+            files: {
+              'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
             }
+          }
+        },
+
+        cssmin : {
+          options: {
+            shorthandCompacting: false,
+            roundingPrecision: -1
+          },
+          target: {
+            files: [{
+              expand: true,
+              cwd: 'public/stylesheets',
+              src: ['*.css', '!*.min.css'],
+              dest: 'dist/css',
+              ext: '.min.css'
+            }]
+          }
         }
     });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin']);
 
 };
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,14 +7,14 @@ $(function() {
     L.esri.basemapLayer("Gray").addTo(map);
 
     var parks = new L.esri.FeatureLayer("http://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Portland_Parks/FeatureServer/0", {
-    style: function () {
-      return { color: "#70ca49", weight: 2 };
-    }
+        style: function () {
+          return { color: "#70ca49", weight: 2 };
+        }
     }).addTo(map);
 
     var popupTemplate = "<h3>{NAME}</h3>{ACRES} Acres<br><small>Property ID: {PROPERTYID}<small>";
 
     parks.bindPopup(function(feature){
-    return L.Util.template(popupTemplate, feature.properties)
+        return L.Util.template(popupTemplate, feature.properties);
     });
 });
